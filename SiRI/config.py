3e20db57c73ea3173a416db9482c3559c3c9fe1e
PostsDesert <gbaker@student.csdspartans.org>
2017-04-07 21:24:58 -0400

    first version

---
+++ SiRI/config.py
@@ -0,0 +1,8 @@
+# Fill in these with your keys and preferences
+
+icloud_email = ''
+icloud_password = ''
+schoology_app_key = ''
+schoology_app_secret = ''
+goo_shorten_url_key = ''
+collection = ''
