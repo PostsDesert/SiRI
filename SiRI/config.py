a15752329f9160512a4993134137ee48d74e7954
Griffith Baker <gbaker@student.csdspartans.org>
2017-04-07 21:53:07 -0400

    Update config.py

--- SiRI/config.py
+++ SiRI/config.py
@@ -1,8 +1,14 @@
-# Fill in these with your keys and preferences
 
+# Apple credentials
 icloud_email = ''
 icloud_password = ''
+
+# Schoology API Key
 schoology_app_key = ''
 schoology_app_secret = ''
+
+# Google link Shortener API Key
 goo_shorten_url_key = ''
+
+# Collection/List that you want your Schoology events to get sent to
 collection = ''
